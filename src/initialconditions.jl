a6e82c0abf1c1f1a430579d0d6d5dc14375a2760
dapias <dandroveros@gmail.com>
2016-08-08 23:35:47 -0500

    Initial version of the program

---
+++ src/initialconditions.jl
@@ -0,0 +1,41 @@
+##Recall that rho(p,q,S) = exp(-¦Â*H)*f(S)
+
+function logisticsampling(Q::Float64)
+    u = rand()
+    s = -log(1./u - 1.) + Q 
+end
+
+function momentum(beta::Float64)
+    p = randn()
+    p = p/sqrt(beta) #Recall that standard deviation is 1/sqrt(beta)
+end    
+    
+
+
+
+
+function boxmuller(beta::Float64) ##For the harmonic oscillator
+    sigma = 1./sqrt(beta)
+    u1 = rand()
+    u2 = rand()
+
+    p = sqrt(-2*log(u1))*cos(2*pi*u2)
+    q = sqrt(-2*log(u1))*sin(2*pi*u2)
+
+    p = p*sigma
+    q = q*sigma
+
+    q,p
+end
+    
+    
+
+function initcond(beta::Float64, Q::Float64)
+    q, p = boxmuller(beta)
+    s = logisticsampling(Q)
+    [q, p, s]
+end
+
+
+
+    
