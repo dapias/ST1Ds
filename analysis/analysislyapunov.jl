0d538197192179973e1a2ccd25a9ac2746be4802
dapias <dandroveros@gmail.com>
2016-08-22 15:32:18 -0500

    Asking input for the potential from the user

--- analysis/analysislyapunov.jl
+++ analysis/analysislyapunov.jl
@@ -1,9 +1,11 @@
 using HDF5
 
-function makelyaparrays(exps::Int64)
+
+
+function makelyaparrays(exps::Int64, potential::AbstractString)
 
     ##Number of simulation files
-    cd("$(homedir())/HooverPrize/data/HO")
+    cd("$(homedir())/HooverPrize/data/$potential")
     dir = filter(x -> endswith(x,".hdf5"), readdir())
     sims = length(dir)
 
@@ -29,12 +31,25 @@ function makelyaparrays(exps::Int64)
     return lyap1, lyap2, lyap3, sims
 end
 
+println("Type the type of the potential (Harmonic oscillator (HO), Mexican Hat (MH), Quartic (Quartic)) ")
+input = string(readline(STDIN))
+potential = input[1:end-1]
+
+potentiallist = ["HO", "MH", "Quartic"]
+
+while !(potential in potentiallist)
+  println("The potential you typed is not in our database. Try one of the following: \n HO, MH or Quartic or check the spelling")
+  input = string(readline(STDIN))
+  potential = input[1:end-1]
+end
+
+
 exponents = 3
-lyap1, lyap2, lyap3, sims  = makelyaparrays(exponents)
+lyap1, lyap2, lyap3, sims  = makelyaparrays(exponents, potential)
 lyapexps = [lyap1, lyap2, lyap3]
 
 cd("$(homedir())/HooverPrize/analysis/")
 
-writedlm("lyapexps.txt", lyapexps)
+writedlm("lyapexps$potential.txt", lyapexps)
 
 ##Open it via readdlm("lyapexps.txt")
