78f39945551ad68d0c72276a9abcf9c60cfd0f2e
dapias <dandroveros@gmail.com>
2016-08-30 09:24:35 -0500

    Updated filename input

--- analysis/analysislyapunov.jl
+++ analysis/analysislyapunov.jl
@@ -9,44 +9,46 @@ using PyPlot
     filen = h5open("../data/potential/filename.hdf5","r")
 """
 
+filename = "gg2cQP"
+potential = filename[end-1:end]
+
 #    Particular example
-file = h5open("../data/HO/UV9nHO.hdf5","r")
-file2 = h5open("../data/HO/8rleHO.hdf5","r")
+file = h5open("../data/$potential/$filename.hdf5","r")
+#file2 = h5open("../data/HO/8rleHO.hdf5","r")
 
 l1 = read(file, "lyapexpsinitcond")
-l2 = read(file2, "lyapexpsinitcond")
+#l2 = read(file2, "lyapexpsinitcond")
 
-l = vcat(l1,l2)
+#l = vcat(l1,l2)
+l = l1
 
+
+try
+    mkdir("../plots")
+end
 
 fig = plt[:figure](figsize=(6,8))
 fig[:subplots_adjust](hspace=.5)
 
 
 ax = fig[:add_subplot](311)
-ax[:set_xlabel](L"$lyap_1$",fontsize="18")
+ax[:set_xlabel](L"$\lambda_1$",fontsize="18")
 #ax[:set_ylabel](L"$p$",fontsize="18")
 ax[:hist](l[:,1])
 
 ax = fig[:add_subplot](312)
-ax[:set_xlabel](L"$lyap_2$",fontsize="18")
+ax[:set_xlabel](L"$\lambda_2$",fontsize="18")
 #ax[:set_ylabel](L"$S$", fontsize="18")
 ax[:hist](l[:,2])
 
 
 ax = fig[:add_subplot](313)
-ax[:set_xlabel](L"$lyap_3$",fontsize="18")
+ax[:set_xlabel](L"$\lambda_3$",fontsize="18")
 #ax[:set_ylabel](L"$S$",fontsize="18")
 ax[:hist](l[:,3])
 
-println("Type a name for the figure ")
-input = string(readline(STDIN))
-filename = input[1:end-1]
-
-try
-    mkdir("../plots")
-end
-
 plt[:savefig]("../plots/$filename.png")
+
+
 
 println("Figure succesfully generated. See file  ../plots/$filename.png")
