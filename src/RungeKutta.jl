ce19342e80853fd38f170b8449b8ec4707efaac1
dapias <dandroveros@gmail.com>
2016-11-10 00:07:19 -0600

    document rungekutta,jl

--- src/RungeKutta.jl
+++ src/RungeKutta.jl
@@ -1,10 +1,14 @@
-function rungeK(x_init::Vector{Float64},campo::Function,h::Float64)
+"""
+    rungeK(xinit, vectorfield, stepsize)
+Classical Runge-Kutta method RK4
+"""
+function rungeK(x_init::Vector{Float64},field::Function,stepsize::Float64)
   state = x_init
-  k1 = campo(state)
-  k2 = campo(state + .5*h*k1)
-  k3 = campo(state + .5*h*k2)
-  k4 = campo(state + h*k3)
+  k1 = field(state)
+  k2 = field(state + .5*stepsize*k1)
+  k3 = field(state + .5*stepsize*k2)
+  k4 = field(state + stepsize*k3)
   phi = (1/6.)*k1 + (1/3.)*k2 + (1/3.)*k3 + (1/6.)*k4
-  state = state+h*phi
+  state = state+stepsize*phi
   return state
 end
