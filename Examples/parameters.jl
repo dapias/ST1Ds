0e49db6a4996f97599e68fdd422313c470cb313b
dapias <dandroveros@gmail.com>
2016-11-29 16:36:47 -0600

    Notebook working

--- Examples/parameters.jl
+++ Examples/parameters.jl
@@ -5,7 +5,7 @@ Q = 0.1
 dtsampling = 0.0125
 dt=0.0025
 nsteps = 50000 #If lyapunov_exponent function is chosen, total time = nsteps*dt
-nsimulations = 10 #If trajectory function is chosen, total time = nsteps*dt*nsimulations. 
+nsimulations = 50 #If trajectory function is chosen, total time = nsteps*dt*nsimulations. 
 thermo = Thermostat("logistic", z-> exp(z/Q)/(Q*( 1 +exp(z/Q))^2.))
 potential = Potential("quartic", x->x^4./4.)
 integrator = Integrator("RK4", ode4)
